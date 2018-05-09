c1ac4d67b437ee402f50edb2b28c7fbae71ba6c5
Heidi Engstrom <hengstrom25@gmail.com>
2018-05-09 15:27:40 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,9 +7,19 @@ class Anagram
     @word = word
   end
   
-  def match(word)
-    
-    
+  result = {}
+
+  words.each do |word|
+    key = word.split('').sort.join
+    if result.has_key?(key)
+      result[key].push(word)
+    else
+      result[key] = [word]
+    end
   end
-  
-end  
+
+  result.each do |k, v|
+   puts "------"
+    p v
+  end
+  end  
